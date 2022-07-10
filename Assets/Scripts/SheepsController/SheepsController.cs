04fd1ffc2a9f0cc14e3392f0e9b169c2a97a21dd
Дэн заборов <denzaborov@Dens-Air.lan>
2022-07-10 20:25:41 +0300

    sheepcontroller

---
+++ Assets/Scripts/SheepsController/SheepsController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepsController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GameObject[] Sheeps = new GameObject[21];
+    public GameObject SheepPrefab;
+    void Start()
+    {
+        for (int i = 0; i < Sheeps.Length; i++)
+        {
+            Sheeps[i] = Instantiate(SheepPrefab, new Vector3(Random.Range(-24, 24), 1f, Random.Range(-24, 24)),Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
